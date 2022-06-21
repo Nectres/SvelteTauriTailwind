e813a2e747629b5c772d8a7d13f8f4f03ee19c62
Keerthi Vasan <sakeerthi23@gmail.com>
2022-06-21 19:54:32 +0530

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let name: string;
+	let count: number = 0;
+</script>
+
+<main>
+	<div class="bg-black text-white">Good morning, {name}</div>
+	<h1 class="text-red-900">Hello {name}!</h1>
+	<p>You have clicked the button: {count} time(s)</p>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={_ => count += 1}>Click</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-black text-white p-5 m-3 rounded-md">hello</div>
